511917f7deee846a23b7bde00f8c62b1936620f9
Avdika <avdukins@gmail.com>
2023-02-23 21:23:53 +0100

    feat: Add second difficalty

--- app.js
+++ app.js
@@ -1,44 +1,34 @@
 let cardAmmount = 6;
-const diffLevelSelect = document.getElementById('difficalty').addEventListener('change', function() {
+const diffLevelSelect = document.getElementById('difficalty').addEventListener('change', function () {
   cardAmmount = +this.value
+  cardArray.length = 0;
+  clearGrid();
+  createCardArray();
+  createBoard();
   return
 });
 
-// const cardAmmount = diffLevelSelect;
-// const diffLevelSelect = document.getElementById('difficalty');
-// let cardAmmount = diffLevelSelect.value ? diffLevelSelect.value : 6;
-// console.log('1 -', cardAmmount)
-
-// diffLevelSelect.addEventListener('change', () => {
-//   cardAmmount = diffLevelSelect.value; // Update the cardAmmount variable whenever the select element changes
-// });
-// console.log('2 -', cardAmmount)
+const gridDisplay = document.querySelector('#grid');
+const resultDisplay = document.querySelector('#result');
 const cardArray = [];
 const selectedCards = [];
 const selectedCardsId = [];
 let pairsFound = cardAmmount;
 
-for (let i = 0; i < 2; i++) {
-  console.log(cardAmmount)
-  for (let n = 0; n < cardAmmount; n++) {
-    cardArray.push(
-      {
-        name: 'Card' + (n + 1),
-        img: 'images/Card' + (n + 1) + '.png',
-      }
-    )
+function createCardArray() {
+  for (let i = 0; i < 2; i++) {
+    for (let n = 0; n < cardAmmount; n++) {
+      cardArray.push(
+        {
+          name: 'Card' + (n + 1),
+          img: 'images/Card' + (n + 1) + '.png',
+        }
+      )
+    }
   }
 }
 
 cardArray.sort(() => 0.5 - Math.random());
-
-console.log('3 -', cardArray);
-
-const gridDisplay = document.querySelector('#grid');
-const resultDisplay = document.querySelector('#result');
-
-// console.log(gridDisplay);
-console.log(cardArray);
 
 function createBoard() {
   for (let i = 0; i < cardArray.length; i++) {
@@ -46,7 +36,6 @@ function createBoard() {
     card.setAttribute('src', 'images/Cover.png');
     card.setAttribute('data-id', i);
     card.addEventListener('click', flipCard);
-    // console.log(card, index);
     gridDisplay.appendChild(card);
   }
   resultDisplay.textContent = cardAmmount;
@@ -55,8 +44,6 @@ function checkForMatch() {
   const cards = document.querySelectorAll('img');
   const optionOneId = selectedCardsId[0];
   const optionTwoId = selectedCardsId[1];
-  // console.log(selectedCards);
-  console.log(pairsFound);
 
   if (selectedCards[0] === selectedCards[1]) {
     cards[optionOneId].setAttribute('src', 'images/White.png');
@@ -71,13 +58,13 @@ function checkForMatch() {
   selectedCards.length = 0;
   selectedCardsId.length = 0;
   resultDisplay.textContent = pairsFound;
-  if  (pairsFound === 0) {
+  if (pairsFound === 0) {
     resultDisplay.textContent = 'Congratulations! You found them all!'
   }
 }
 
 function flipCard() {
-  const elementId  = this.getAttribute('data-id');
+  const elementId = this.getAttribute('data-id');
 
   selectedCards.push(cardArray[elementId].name);
   selectedCardsId.push(elementId);
@@ -88,4 +75,10 @@ function flipCard() {
   }
 }
 
+function clearGrid() {
+  const childs = gridDisplay.querySelectorAll('img');
+    childs.forEach((child) => child.remove());
+}
+
+createCardArray();
 createBoard();
